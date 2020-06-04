b4a26a7062ad5acb729666b803bff2a4297ab2ed
julienne <julienne.codes@gmail.com>
2020-06-04 18:26:10 -0400

    Create 6zp Simple Iterator.js
    - question on line 13 - 14

---
+++ 6zp Simple Iterator.js
@@ -0,0 +1,34 @@
+//Simple iterator
+//Simple iterator returns an object
+//Returned object has a next method
+//next method returns an object with properties of value and done
+function makeIterator(array){
+  let nextIndex = 0;
+
+  return {
+    next(){
+      if(nextIndex < array.length){
+        return {
+          value: nextIndex++,
+          //Shouldn't this evaluate to 1?
+          //However, when I call the next method and it returns value: 0
+
+          done: false
+        }
+      }
+
+      else {
+        return {
+          done: true
+        }
+      }
+    }
+  }
+}
+
+let dessertIterator = makeIterator(['cookie', 'cake', 'chocolate']);
+//Calling next method
+dessertIterator.next();//{value: 0, done: false}
+dessertIterator.next();//{value: 1, done: false}
+dessertIterator.next();//{value: 2, done: false}
+dessertIterator.next();//{done: true}
