988ee282b1b5b102cd752047a012e5b63d4b2399
julienne <julienne.codes@gmail.com>
2020-07-04 19:48:59 -0400

    Update 8z errors4.js

--- 8z errors4.js
+++ 8z errors4.js
@@ -59,27 +59,14 @@ let dirtyJob = new Book("A Dirty Job");
 addNewBooksToLibrary(dirtyJob);
 
 //------------------------------------------------------------------------------
-console.log(libraryAccounts);
-console.log(books);
-
-//------------------------------------------------------------------------------
 function isRegisteredAccount(account){
   try {
     if (libraryAccounts.includes(account)) return true;
     else throw new Error();
   } catch(e){
-    return `${account.name} is not registered in this library. Please talk to the librarian to open a new library account.`;
+    throw new Error(`${account.name} is not registered in this library. Please talk to the librarian to open a new library account.`);
   }
 }
-//testing
-//happy scenario
-isRegisteredAccount(giJoe);
-console.log(isRegisteredAccount(giJoe));
-
-//error scenario
-let lizLemon = createNewAccount("Liz Lemon", 203003);
-isRegisteredAccount(lizLemon);
-console.log(isRegisteredAccount(lizLemon));
 
 //------------------------------------------------------------------------------
 function isOwnedByLibrary(book){
@@ -88,19 +75,9 @@ function isOwnedByLibrary(book){
     else throw new Error();
 
   } catch(e){
-    return `Unfortunately, ${book.title} is not currently owned by the library.`;
+    throw new Error(`Unfortunately, ${book.title} is not currently owned by the library.`);
   }
 }
-
-//testing
-//happy scenario, book is owned by the library
-isOwnedByLibrary(artemisFowl);
-console.log(isOwnedByLibrary(artemisFowl));
-
-//error scenario, book is not owned by the library
-let designOfEveryDayThings = new Book("Design of Everyday Things");
-isOwnedByLibrary(designOfEveryDayThings);
-console.log(isOwnedByLibrary(designOfEveryDayThings));
 
 //------------------------------------------------------------------------------
 function isAvailableForLoan(book){
@@ -108,64 +85,53 @@ function isAvailableForLoan(book){
     if(book.available === true) return true;
     else throw new Error();
   }catch(e){
-    return `Unfortunately, ${book.title} is currently checked out. Please try again later.`;
+    throw new Error(`Unfortunately, ${book.title} is currently checked out. Please try again later.`);
   }
 }
 
-//testing
-//happy scenario, book can be checked out
-isAvailableForLoan(artemisFowl);
-console.log(isAvailableForLoan(artemisFowl));
-
-//error scenario, book is not available for loan
-let hitchhikers = new Book("The Hitchhiker's Guide to the Galaxy");
-addNewBooksToLibrary(hitchhikers);
-hitchhikers.borrow();
-isAvailableForLoan(hitchhikers);
-console.log(isAvailableForLoan(hitchhikers));
-
 //------------------------------------------------------------------------------
+//this isn't working, duplicates are still being allowed
 function canBeBorrowedByAccount(account, book){
   try {
     if(!account.borrowedBooks.includes(book)) return true;
     else throw new Error();
   } catch(e){
-    return `${book.title} is already loaned to your account.`;
+    throw new Error(`${book.title} is already loaned to your account.`);
+  }
+}
+//------------------------------------------------------------------------------
+function borrowBook(account, book){
+  try{
+    isRegisteredAccount(account);
+    isOwnedByLibrary(book);
+    isAvailableForLoan(book);
+    canBeBorrowedByAccount(account, book);
+
+    // account.borrowedBooks.push(book);
+    // book.borrow();
+    console.log("Book borrowed");
+  } catch(e){
+    console.log(e);
   }
 }
 
-//happy scenario, book is not yet loaned to account
-canBeBorrowedByAccount(giJoe, harryPotter);
-console.log(canBeBorrowedByAccount(giJoe, harryPotter));
-
-//error scenario, book is already loaned to account
-lemonySnicket.borrowedBooks = lemonySnicket.borrowedBooks.push(ersatzElevator);
-console.log(lemonySnicket.borrowedBooks);
-canBeBorrowedByAccount(lemonySnicket, ersatzElevator);
-console.log(canBeBorrowedByAccount(lemonySnicket, ersatzElevator));
-
-//------------------------------------------------------------------------------
-function borrowBook(account, book){
-  //check if account exists
-  isRegisteredAccount(account);
-
-  //check if book exists
-  isOwnedByLibrary(book);
-
-  //check if the book is available
-  isAvailableForLoan(book);
-
-  //check if book is already borrowed by account
-  canBeBorrowedByAccount(account, book);
-
-  //happy scenario, book is borrowed
-  return account.borrowedBooks.push(book);
-}
-
 //happy scenario
-//existing account
-//existing book
-//available book
-//book not yet borrowed by account
 borrowBook(giJoe, dirtyJob);
-console.log(giJoe.borrowedBooks);
+
+//error scenario - unregistered account
+let lizLemon = createNewAccount("Liz Lemon", 203003);
+borrowBook(lizLemon, dirtyJob);
+
+//error scenario - book not owned by library
+let designOfEveryDayThings = new Book("Design of Everyday Things");
+borrowBook(giJoe, designOfEveryDayThings);
+
+//error scenario - book not available
+let hitchhikers = new Book("The Hitchhiker's Guide to the Galaxy");
+addNewBooksToLibrary(hitchhikers);
+hitchhikers.borrow();
+borrowBook(giJoe, hitchhikers);
+
+//error scenario - book already borrowed by account
+giJoe.borrowedBooks = giJoe.borrowedBooks.push(dirtyJob);
+borrowBook(giJoe, dirtyJob);
